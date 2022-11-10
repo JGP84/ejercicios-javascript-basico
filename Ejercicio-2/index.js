4500cd3555ac1a78fc2a5c779d6914e2faa5d098
José <jose@MacBook-Air-de-Usuario.local>
2022-11-10 21:52:10 +0100

    ejercicio 2

---
+++ Ejercicio-2/index.js
@@ -0,0 +1,17 @@
+
+const birthday = new Date('january 19 1984')
+const publicationDate = new Date('march 20 1967')
+
+const lista = [
+  "José",
+  38,
+  true,
+  birthday,
+  {
+    titulo: "100 años de soledad",
+    autor: "Gabriel García Marquez",
+    fecha: publicationDate,
+    url: "https://www.gavilan.edu/academic/spanish/gaspar/html/24_02.html#:~:text=Considerada%20una%20obra%20maestra%20de,de%201967%20en%20Buenos%20Aires.",
+  },
+];
+
